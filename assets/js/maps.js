a8d0945b7426b2f4ecbe1f6947b24dc0a1fba4c7
Harry Sibbenga <sibbengaharry@gmail.com>
2018-07-18 21:10:55 +0000

    updated intrests.html

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,18 +1,19 @@
 function initMap() {
     var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 3,
+        zoom: 2,
         center: {
-            lat: 46.619261,
-            lng: -33.134766
+            lat: 18.5331131,
+            lng: 0.0646187
         }
     });
 
     var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
     var locations = [
-        { lat: 40.785091, lng: -73.968285 },
-        { lat: 41.084045, lng: -73.874245 },
-        { lat: 40.754932, lng: -73.984016 }
+        { lat: 52.0406224, lng: -0.7594171 },
+        { lat: 52.9172085, lng: -1.1937813 },
+        { lat: -17.9427095, lng: 25.7432598 },
+        { lat: 24.417293, lng: 54.4611633 }
     ];
 
     var markers = locations.map(function(location, i) {
